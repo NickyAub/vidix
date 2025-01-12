9d13c85d2405f736cc3ea3093ae1775bea2f2f8a
Nicolas AUBERVAL <nicauberval+gh@gmail.com>
2025-01-12 13:29:04 +0100

    Paginate movies + sort columns + Homepage controller TODO

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'app.movie')]
-    public function index(MovieRepository $repository): Response
+    public function index(MovieRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        $movies = $repository->findAll();
+        $movies = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+    
         return $this->render('views/movie/index.html.twig', [
             'movies' => $movies,
         ]);
